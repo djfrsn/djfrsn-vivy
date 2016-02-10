40fb8a3fceae77792503b7e4a6c17406754432e8
djfrsn <dj@dennisjefferson.com>
2016-02-09 18:19:16 -0800

    Set key to var

--- newrelic.js
+++ newrelic.js
@@ -4,6 +4,7 @@
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
+ var NEW_RELIC_LICENSE_KEY = NEW_RELIC_LICENSE_KEY || '';
 exports.config = {
   /**
    * Array of application names.
