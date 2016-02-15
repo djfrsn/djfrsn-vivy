57152def44c1f73e442ab512ef679541a50d537d
djfrsn <dj@dennisjefferson.com>
2016-02-14 17:13:43 -0800

    Add header styles & update component

--- app/containers/Navigation.jsx
+++ app/containers/Navigation.jsx
@@ -2,19 +2,24 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { logOut } from 'actions/users';
+import classNames from 'classnames/bind';
 import styles from 'scss/components/_navigation';
+
+const cx = classNames.bind(styles);
 
 class Navigation extends Component {
 
   render() {
     const { dispatch } = this.props;
     return (
-      <header className={styles.navigation}>
-        <nav className={styles.navigation} role="navigation">
-          <Link to="/" className={styles.navigation__item + ' ' + styles['navigation__item--logo']} activeClassName={styles['navigation__item--active']}>DJ</Link>
-          <Link className={styles.navigation__item} to="/journal">Journal</Link>
-          <Link to="/about" className={styles.navigation__item} activeClassName={styles['navigation__item--active']}>About</Link>
-          <Link className={styles.navigation__item} to="/projects">Projects</Link>
+      <header className={cx('bp-header')}>
+        <span>Site Title <span className={cx('bp-icon', 'bp-icon-about', 'site-info-button')} data-content="Site Info"></span></span>
+        <div className={cx('site-info')}>Site Info</div>
+        <h1>Site Message</h1>
+        <nav role="navigation">
+          <a className={cx('bp-icon', 'bp-icon-child', 'hire_me_button')} data-info="Hire Me"><span>Hire Me</span></a>
+          <a href="Twitter user url" className={cx('bp-icon', 'bp-icon-twitter')} data-info="Site twitterusername" target="_blank"><span>@twitter.name</span></a>
+          <a href="mailto:Site Email" className={cx('bp-icon', 'bp-icon-email')} data-info="send me e-mail"><span>send me e-mail</span></a>
         </nav>
       </header>
     );
@@ -34,3 +39,10 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(Navigation);
+
+// <nav className={styles.navigation} role="navigation">
+//   <Link to="/" className={styles.navigation__item + ' ' + styles['navigation__item--logo']} activeClassName={styles['navigation__item--active']}>DJ</Link>
+//   <Link className={styles.navigation__item} to="/journal">Journal</Link>
+//   <Link to="/about" className={styles.navigation__item} activeClassName={styles['navigation__item--active']}>About</Link>
+//   <Link className={styles.navigation__item} to="/projects">Projects</Link>
+// </nav>
