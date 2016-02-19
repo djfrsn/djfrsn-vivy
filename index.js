969bf2959d1c2560c540a04c2254ffcf32e4875d
djfrsn <dj@dennisjefferson.com>
2016-02-18 19:55:02 -0800

    Add comment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('babel-core/register');
+require('babel-core/register'); // excluding node_modules, all js 'required' after this is transformed by Babel
 require('babel-polyfill');
 require('css-modules-require-hook');
 require('./server/server');
