ffdd5a159dc68f0e6f967509c991b71106c97767
Dennis Jefferson <dennismjefferson@gmail.com>
2016-02-27 13:23:07 -0800

    Update title & remove unused link

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import classNames from 'classnames/bind';
-import { Link } from 'react-router';
 import styles from './Header.scss';
 
 const cx = classNames.bind(styles);
@@ -12,7 +11,7 @@ class Navigation extends React.Component {
       <header className={cx('bp-header')}>
         <span>Dennis Jefferson<span className={cx('bp-icon', 'bp-icon-about', 'site-info-button')} data-content="Rwarr"></span></span>
         <div className={cx('site-info')}>Dev since 12</div>
-        <h1>Full Stack Developer</h1>
+        <h1>Front-End Engineer</h1>
         <nav role="navigation">
           <a className={cx('bp-icon', 'bp-icon-child', 'hire_me_button')} data-info='Hire Me'><span>Hire Me</span></a>
           <a href="Twitter user url" className={cx('bp-icon', 'bp-icon-twitter')} data-info='kaw kaw' target='_blank'><span>@twitter.name</span></a>
