1a64614cb2194de16b9739c29009e9fb47bbeb71
Dennis Jefferson <dennismjefferson@gmail.com>
2016-02-27 16:25:13 -0800

    Add navigate logic sans element retrieval

--- app/containers/Slider/Slider.jsx
+++ app/containers/Slider/Slider.jsx
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import Slide from 'Slide/Slide';
 import SliderNav from 'SliderNav/SliderNav';
 import classNames from 'classnames/bind';
+import dynamics from 'vendor/dynamics';
 import styles from './Slider.scss';
 
 const cx = classNames.bind(styles);
@@ -17,7 +18,52 @@ export default class Slider extends Component {
   }
 
   navigate = (dir) => {
+    let currentEl;
+    let itemCurrent;
+    let currentTitleEl;
+    let nextTitleEl;
+    let nextEl;
+    let itemNext;
+        // animate the current element out
+    dynamics.animate(currentEl, { opacity: 0, translateX: dir === 'right' ? -1 * currentEl.offsetWidth / 2 : currentEl.offsetWidth / 2, rotateZ: dir === 'right' ? -10 : 10 }, {
+      type: dynamics.spring,
+      duration: 3000,
+      friction: 600,
+      complete: () => {
+        dynamics.css(itemCurrent, { opacity: 0, visibility: 'hidden' });
+      }
+    });
 
+    // animate the current title out
+    dynamics.animate(currentTitleEl, { translateX: dir === 'right' ? -250 : 250, opacity: 0 }, {
+      type: dynamics.bezier,
+      points: [{'x': 0, 'y': 0, 'cp': [{'x': 0.2, 'y': 1}]}, {'x': 1, 'y': 1, 'cp': [{'x': 0.3, 'y': 1}]}],
+      duration: 450
+    });
+
+    // set the right properties for the next element to come in
+    dynamics.css(itemNext, { opacity: 1, visibility: 'visible' });
+    dynamics.css(nextEl, { opacity: 0, translateX: dir === 'right' ? nextEl.offsetWidth / 2 : -1 * nextEl.offsetWidth / 2, rotateZ: dir === 'right' ? 10 : -10 });
+
+    // animate the next element in
+    dynamics.animate(nextEl, { opacity: 1, translateX: 0 }, {
+      type: dynamics.spring,
+      duration: 3000,
+      friction: 600
+      // complete: () => {
+      //   items.forEach(function(item) { classie.remove(item, 'slide--current'); });
+      //   classie.add(itemNext, 'slide--current');
+      // }
+    });
+
+    // set the right properties for the next title to come in
+    dynamics.css(nextTitleEl, { translateX: dir === 'right' ? 250 : -250, opacity: 0 });
+    // animate the next title in
+    dynamics.animate(nextTitleEl, { translateX: 0, opacity: 1 }, {
+      type: dynamics.bezier,
+      points: [{'x': 0, 'y': 0, 'cp': [{'x': 0.2, 'y': 1}]}, {'x': 1, 'y': 1, 'cp': [{'x': 0.3, 'y': 1}]}],
+      duration: 650
+    });
   }
 
   render() {
